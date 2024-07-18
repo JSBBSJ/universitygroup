a1b7796e4bd435a127acfbc8c013c653f6751930
kyj <kyj001208@naver.com>
2024-07-18 17:41:18 +0900

    feat: backend start

--- src/main/java/com/green/universityGroup/controller/StudentController.java
+++ src/main/java/com/green/universityGroup/controller/StudentController.java
@@ -1,14 +1,31 @@
 package com.green.universityGroup.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class StudentController {
 
+import com.green.universityGroup.service.StudentService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class StudentController {
+	
+	private final StudentService service;
+
+	/*
+	 * @GetMapping("/student") public String main() { return
+	 * "/views/student/s-main"; }
+	 */
+		
+		//db 이미 저장된 값 저장된 내용 조회화면 불러오기
 		@GetMapping("/student")
-		public String main() {
-			return "/views/student/s-main";
+		public String check(Model model) {
+			
+			service.SlistProcess(model);
+			return "views/student/s-main";
 		}
 		
 }
